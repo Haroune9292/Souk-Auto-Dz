b5e160bd6b96fa8e12a6c76e3a48d5415993e63a
Haroune <haroune7@gmail.com>
2026-08-20 17:36:13 +0100

    update all

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css';
 import { LanguageProvider } from '@/lib/context/LanguageContext';
+import Navbar from '@/components/NAVBAR'; // استيراد النافبار
+
 export const metadata = {
   title: 'Souk Auto Dz',
   description: 'Algeria Car Classifieds Marketplace',
@@ -14,6 +16,7 @@ export default function RootLayout({
     <html lang="fr" dir="ltr">
       <body>
         <LanguageProvider>
+          <Navbar /> {/* عرض النافبار هنا ليظهر في كل الصفحات */}
           {children}
         </LanguageProvider>
       </body>
